a1e6bb7ccb8146c7bb6c7cf6b67a33cd71400f43
awnishbhatt <awnishbhatt@yahoo.com>
2021-05-29 17:28:47 -0500

    -App.js   - lifting the state data to new parent - App.js is the new parent that contains the navbar.jsx and counters.jsx   - also defined the state in this class and all event handlers from the child classes to be bubbled up to this class and execute here
    - counters.jsx
     - corresponding changes to use props in this child class of App.js
    
    - navbar.jsx
      - copied from bootstrap navbar
    
    - index.js
      - now renders the new parent App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/navbar";
+import Counters from "./components/counters";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+
+//Modified the App to be the contaier for the Navbar + the Counters
+class App extends Component {
+
+    state = {
+        counters: [
+            {id: 1, value: 44},
+            {id: 2, value: 0},
+            {id: 3, value: 0},
+            {id: 4, value: 0}
+        ]
+    }
+
+    handleIncrement = counter => {
+        console.log("handle increment called", counter);
+
+        const newCounters = [...this.state.counters]; // cloning the counters
+        const indexOfCounter = newCounters.indexOf(counter);
+        newCounters[indexOfCounter] = {...counter};
+        newCounters[indexOfCounter].value++;
+        this.setState({counters: newCounters});
+    };
+
+    handleDelete = (counterId) => {
+        console.log("handle delete called", counterId);
+        //note: you done delete the element from the state, but instead assign a new state variable for reach to then refresh
+
+        const localCounter = this.state.counters.filter(cntr => cntr.id !== counterId);
+        this.setState({counters: localCounter});
+    };
+
+    handleReset = () => {
+        const newCounters = this.state.counters.map(theCounter => {
+            theCounter.value = 0;
+            return theCounter;
+        })
+        this.setState({counters: newCounters});
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar/>
+                <main className="container">
+                    <Counters
+                        parentCounters={this.state.counters}
+                        onReset={this.handleReset}
+                        onDelete={this.handleDelete}
+                        onIncrement={this.handleIncrement}>
+                    </Counters>
+                </main>
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,52 +5,20 @@ import Counter from "./counter";
 // Moved handleIncrement, added handleReset and change this class to control and maintain state in this class only. While the events (onClick) is defined in the "Counter" class,
 // its eventhandler "handleIncrement etc.." are defined and the counter state modified in this class.
 class Counters extends Component {
-    state = {
-        counters: [
-            {id: 1, value: 44},
-            {id: 2, value: 0},
-            {id: 3, value: 0},
-            {id: 4, value: 0}
-        ]
-    }
 
-    handleIncrement = counter => {
-        console.log("handle increment called", counter);
-
-        const newCounters = [...this.state.counters]; // cloning the counters
-        const indexOfCounter = newCounters.indexOf(counter);
-        newCounters[indexOfCounter] = {...counter};
-        newCounters[indexOfCounter].value++;
-        this.setState({counters : newCounters});
-    };
-
-    handleDelete = (counterId) => {
-        console.log("handle delete called", counterId);
-        //note: you done delete the element from the state, but instead assign a new state variable for reach to then refresh
-
-        const localCounter = this.state.counters.filter(cntr => cntr.id !== counterId);
-        this.setState({counters: localCounter});
-    };
-
-    handleReset = () => {
-        const newCounters = this.state.counters.map(theCounter => {
-            theCounter.value =0;
-            return theCounter;
-        })
-        this.setState({counters: newCounters});
-    };
-
-    //note: key is a special attr that identifies the component and other properties of the component are  seen in "this.props"
+     //note: key is a special attr that identifies the component and other properties of the component are  seen in "this.props"
     render() {
         return (
             <div>
-                <button className="btn btn-sm m-2 btn-primary" onClick={this.handleReset}>Reset</button>
-                {this.state.counters.map(counter =>
+                <button className="btn btn-sm m-2 btn-primary"
+                        onClick={this.props.onReset}>Reset</button>
+                {
+                    this.props.parentCounters.map(counter =>
                     <Counter
                         key={counter.id}
                         counter={counter}
-                        onMyDelete={this.handleDelete}
-                        onMyIncrement={this.handleIncrement}
+                        onMyDelete={this.props.onDelete}
+                        onMyIncrement={this.props.onIncrement}
                     />)
                 }
             </div>
